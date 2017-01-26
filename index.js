265f88f48a173c9a240969689f7ee46e420a2c98
ThomDo <tdonath@libri.de>
2017-01-26 10:09:30 +0100

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var os = require('os');
+var app = express();
+
+let port = process.env.APP_PORT || 8080
+let message = process.env.APP_MESSAGE || 'default message'
+
+app.get('/', function (req, res) {
+  res.send(`
+<!DOCTYPE html>
+<head title="TestApplikation">
+</head>
+<body>
+    <h1>Moin Moin :)</h1>
+    Die Testapp läuft auf <b>${os.hostname()}</b> an Port <b>${port}</b> mit dem Text <b>"${message}"</b>
+</body>
+</html>`
+
+  );
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
+
